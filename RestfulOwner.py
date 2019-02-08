87c221da40be9dcdeb8b0da6fb727116b1b16438
FlorianGelb <Florian.Braun02@gmail.com>
2019-02-08 13:17:44 +0100

    Cleaned code

--- RestfulOwner.py
+++ RestfulOwner.py
@@ -1,24 +1,31 @@
 import requests
 
+
 class RestfulOwner:
     def __init__(self):
         self.host = "http://127.0.0.1:5000/"
         self.ID = "owner"
+        self.clients = []
 
         if self.check_server():
             self.run()
 
     def check_server(self):
         request = requests.get(self.host)
-        if request.ok and request.json()["Info"] =="RESTFUL_MORDAX":
+        if request.ok and request.json()["info"] == "RESTFUL_MORDAX":
             return True
+
     def get_clients(self):
-        request = requests.post(self.host, json={"KEY":self.ID, "CMD":"GET_CLNTS"})
+        request = requests.post(self.host, json={"KEY": self.ID, "CMD": "GET_CLNTS"})
         if request.ok:
             return request.json()
 
     def run(self):
-        if self.get_clients()["Clients"] != None:
+
+        if self.get_clients()["Clients"] is not None:
             for i in range(len(self.get_clients()["Clients"])):
+                self.clients.append(self.get_clients()["Clients"][i])
                 print("Client: {}".format(self.get_clients()["Clients"][i]))
+
+
 r = RestfulOwner()
--- RestfulServer.py
+++ RestfulServer.py
@@ -2,23 +2,25 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 import random
 
+
 class RestfulServer(Resource):
     task_list = []
     client_list = []
-    def __init__ (self):
+    ID_CNT = 0
+    MAX_CLIENTS = 1000
+    ID = None
+    CMD = None
+
+    def __init__(self):
         self.app = Flask(__name__)
         self.api = Api(self.app)
         self.api.add_resource(RestfulServer, "/")
 
     def start(self):
-        self.task_list = []
-        self.client_list = []
-        self.ID_CNT = 0
-        self.MAX_CLIENTS = 10000
         self.app.run()
 
-
-    def get(self):
+    @staticmethod
+    def get():
         return {"Info": "RESTFUL_MORDAX"}
 
     def post(self):
@@ -26,67 +28,67 @@ class RestfulServer(Resource):
         task_list_total = []
         client_list_total = []
 
-        if self.get_KEY(json) == "owner" and self.get_COMMAND(json) != "GET_CLNTS" and len(self.task_list) != 0:
+        if self.get_key(json) == "owner" and self.get_command(json) != "GET_CLNTS" and len(self.task_list) != 0:
             for i in range(len(self.task_list)):
                 task_list_total.append(self.task_list[i])
             return {"Task": task_list_total}
 
-        if self.get_KEY(json) == "owner" and self.get_COMMAND(json) == "GET_CLNTS":
+        if self.get_key(json) == "owner" and self.get_command(json) == "GET_CLNTS":
             if len(self.client_list) != 0:
                 for i in range(len(self.client_list)):
                     client_list_total.append(self.client_list[i])
-                return {"Clients":client_list_total}
+                return {"Clients": client_list_total}
             else:
-                return{"Clients":"None"}
+                return{"Clients": "None"}
 
-        if len(self.task_list) != 0 and self.get_KEY(json) != None:
+        if len(self.task_list) != 0 and self.get_key(json) is not None:
             for i in range(len(self.task_list)):
-                if self.get_KEY(json) in self.task_list[i]:
-                    return{"Task":self.task_list[i]}
+                if self.get_key(json) in self.task_list[i]:
+                    return{"Task": self.task_list[i]}
 
-        if self.get_ID(json) == None and self.get_COMMAND(json) == "ID_RQST":
-            new_ID = self.check_ID(random.randrange(10000))
-            return {"NewID":new_ID}
+        if self.get_id(json) is None and self.get_command(json) == "ID_RQST":
+            new_id = self.check_id(random.randrange(10000))
+            return {"NewID": new_id}
 
     def put(self):
         json = request.get_json()
-        if self.get_KEY(json) == "owner":
-            self.ID = self.get_ID(json)
-            self.CMD = self.get_COMMAND(json)
+        if self.get_key(json) == "owner":
+            self.ID = self.get_id(json)
+            self.CMD = self.get_command(json)
             self.task_list.append('{' + '"Task":"{}","ID":"{}"'.format(self.CMD, self.ID) + '}')
-            return {"tasks":self.task_list}
+            return {"tasks": self.task_list}
 
     @staticmethod
-    def get_KEY(json):
+    def get_key(json):
         try:
             return json["KEY"]
-        except Exception as e:
+        except KeyError:
             return None
 
     @staticmethod
-    def get_COMMAND(json):
+    def get_command(json):
         try:
             return json["CMD"]
-        except Exception as e:
+        except KeyError:
             return None
 
     @staticmethod
-    def get_ID(json):
+    def get_id(json):
         try:
             return json["ID"]
-        except Exception as e:
+        except KeyError:
             return None
 
-    def check_ID(self, ID):
+    def check_id(self, new_id):
         if len(self.client_list) != 0:
-            if ID in self.client_list:
-                self.check_ID(random.randrange(self.MAX_CLIENTS))
+            if new_id in self.client_list:
+                self.check_id(random.randrange(self.MAX_CLIENTS))
             else:
-                self.client_list.append(ID)
-                return ID
+                self.client_list.append(new_id)
+                return new_id
         else:
-            self.client_list.append(ID)
-            return(ID)
+            self.client_list.append(new_id)
+            return new_id
 
 
 r = RestfulServer()
